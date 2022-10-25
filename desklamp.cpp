ca8ed95733e29bb1533ff45fb5c6cd7583366665
clnmcdowell <114112169+clnmcdowell@users.noreply.github.com>
2022-10-25 17:45:35 -0400

    Added brightness control

--- desklamp.cpp
+++ desklamp.cpp
@@ -4,7 +4,9 @@
 #define power 8
 #define button 4
 #define ledCount 16
+#define slide A0
 int on;
+double aread,brightness, x = (-13689.0/41.0);
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(ledCount, led, NEO_RGBW + NEO_KHZ800);
 
@@ -17,10 +19,13 @@ void setup() {
  
 void loop() {
   on = digitalRead(power);
+  aread = analogRead(slide);
+  brightness = ((aread + x)/(1023+ x))*255;
+  
 
 if(on == HIGH){ 
  for(int i = 0; i < ledCount; i++){
-    pixels.setPixelColor(i, pixels.Color(0,0,0,255));
+    pixels.setPixelColor(i, pixels.Color(0,0,0,brightness));
     pixels.show(); 
     on = digitalRead(power);
  }
